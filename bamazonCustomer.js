7b91542927c5d63aeac70bdeb59f166172f20095
Joe <dinhx068@umn.edu>
2018-09-28 15:25:48 -0500

    done, could have some improvements but it is what it is

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,6 +20,7 @@ function validateNumber(input) {
     return ( (reg.test(input)) && (input > 0) ) || "Enter a valid number!";
 }
 
+// Notice the -1 instead of 0
 function validateNumber2(input) {
     var reg = /^\d+$/;
     return ( (reg.test(input)) && (input > -1) ) || "Enter a valid number!";
@@ -50,7 +51,6 @@ function start() {
         type: "input", // Grabbing Item id
         message: "What would you like to buy? Enter the Item ID of that product.",
         validate: validateNumber
-
       }, {
         name: "AMOUNT",
         type: "input", // Grabbing amount that the customer wants
@@ -68,31 +68,63 @@ function start() {
 function purchase(itemIdSelected,amountSelected){
     connection.query('SELECT * FROM Products', function(err, res) {
         if (err) {console.log(err)};
-        // When the user enters an amount less than what is in stock
-        if (amountSelected > res[itemIdSelected].stock) {
-            console.log(`You cannot buy that many of ${res[itemIdSelected].product_name} since we do not have that many in stock.`);
-            inquirer.prompt({
-                  name: "AMOUNT",
-                  type: "input", // Grabbing user amount
-                  message: "--------------------------------------------------------------------------------------------------" + 
-                  "\nSo, enter an amount that is equal to or less than " + res[itemIdSelected].stock + " for " + res[itemIdSelected].product_name + "." +
-                  "\nOr enter 0 to go back to the select another product.",
-                  validate: validateNumber2
-                })
-                .then(function(answer) {
-                    let newAmount = answer.AMOUNT; // New input from the user
-                    // If user wants to go back to the table
-                    if (0 == newAmount) { 
-                        setTimeout(displayTable, 1000);
-                    // Get valid number from the user by rerunning the function
-                    } else {
-                        purchase(itemIdSelected,newAmount);
-                    }
-                });
-        // When user enters a valid input we go here
+
+        // Making an product selected object so it's easier to access certain things
+        var productSelected = {
+            id: res[itemIdSelected].item_id,
+            name: res[itemIdSelected].product_name,
+            department_name: res[itemIdSelected].department_name,
+            price: res[itemIdSelected].price,
+            stock: res[itemIdSelected].stock
+        };
+
+        if (productSelected.stock === 0) {
+            console.log(`You cannot buy any units of ${productSelected.name} because there is 0 in stock.`);
+            setTimeout(function () {
+                console.log(`You will be returned to the catalog shortly!`)}, 
+                3000);
+            setTimeout(displayTable, 5000);
         } else {
-
+            // When the user enters an amount less than what is in stock
+            if (amountSelected > productSelected.stock) {
+                console.log("----------------------------------------------------------------------------------------------------" + 
+                "\nYou cannot buy that many of [" + productSelected.name + "] since we do not have that many in stock.");
+                inquirer.prompt({
+                    name: "AMOUNT",
+                    type: "input", // Grabbing user amount
+                    message: "So, enter an amount that is equal to or less than [" + productSelected.stock + "] for [" + productSelected.name + "]." +
+                    "\nOr enter 0 to go back to the select another product.",
+                    validate: validateNumber2 // Using validateNumber function but with 0 included
+                    })
+                    .then(function(answer) {
+                        let newAmount = answer.AMOUNT; // New input from the user
+                        // If user wants to go back to the table
+                        if (0 == newAmount) { 
+                            setTimeout(displayTable, 1000);
+                        // Get valid number from the user by rerunning the function
+                        } else {
+                            purchase(itemIdSelected, newAmount);
+                        }
+                    });
+            // When user enters a valid input we go here
+            } else {
+                let newStockCount = productSelected.stock - amountSelected;
+                let totalCost = amountSelected * productSelected.price;
+                connection.query("UPDATE products SET ? WHERE ?",
+                    [
+                        { stock: newStockCount },
+                        { item_id: productSelected.id }
+                    ],
+                    function(error) {
+                        if (error) throw err;
+                        console.log(`Buying ${amountSelected} unit(s) of ${productSelected.name} will cost you $${totalCost}.`); 
+                        setTimeout(function () {
+                            console.log(`Thanks for shopping at Bamazon! \nYou will be returned to the catalog shortly!`)}, 
+                            3000);
+                        setTimeout(displayTable, 5000);
+                    }
+                );
+            }
         }
-
     });
 }
